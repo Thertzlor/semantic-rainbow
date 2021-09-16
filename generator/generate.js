39714d45c642a4f27c474c6330b611f9e7a28d12
Timo Hertzler <timo.hertzler@gmail.com>
2021-09-16 16:40:14 +0200

    local commit

---
+++ generator/generate.js
@@ -0,0 +1,34 @@
+//This file is used 
+import tinycolor from "tinycolor2";
+import { generateColors } from "./lib/colorGenerator.js";
+import {readFileSync,writeFile} from "fs";
+
+const configPath = './themes/Semantic Rainbow-color-theme.json'
+const cnfString =readFileSync(configPath,'utf-8')
+const cnf = JSON.parse(cnfString);
+const colorConfig = JSON.parse(readFileSync('./generator/config.json','utf-8'))
+
+const  {semanticRules, fallBackRules} = generateColors(tinycolor,colorConfig);
+const {tokenColors} = cnf;
+
+fallBackRules.forEach(({scope})=>
+ tokenColors.map((r,i)=>(typeof r.scope === 'string'?scope.includes(r.scope):r.scope.some(i => scope.includes(i)))?{i,r}:null).filter(f=>f).sort((a,b)=>a.i<b.i?1:-1).forEach(({r,i})=>{
+       if(scope.includes(r.scope)) return tokenColors.splice(i,1);
+       if(scope.every(i=>r.scope.includes(i) && (r.scope.length === scope.length))) return tokenColors.splice(i,1);
+       scope.forEach(s=>{
+          const subIndex = r.scope.findIndex(e=>e===s);
+          if(subIndex !== -1) tokenColors[i].scope = tokenColors[i].scope.splice(subIndex,1)
+       })
+    })
+)
+
+//Deleting the "alias" property from rule definitions
+for (const k in semanticRules)((Object.hasOwnProperty.call(semanticRules, k)) && semanticRules[k].alias && delete semanticRules[k].alias)
+
+cnf.semanticTokenColors = semanticRules;
+cnf.tokenColors = tokenColors.concat(fallBackRules);
+
+const stringRules = JSON.stringify(cnf)
+
+if(stringRules !== cnfString)writeFile(configPath,JSON.stringify(cnf),'utf8',()=>{console.log('saved config.')})
+export {}
---
+++ generator/lib/colorGenerator.js
@@ -0,0 +1,79 @@
+function generateColors(tinycolor,config){
+   const semanticRules={};
+   const fallBackRules=[];
+   const baseNumber = Object.keys(config.baseTokenColors).length;
+   const filterNumber = Object.keys(config.filters).length;
+   const extraCombinationsNumber = config.modifierCombinations.length
+   const numColors = (baseNumber*filterNumber)+(baseNumber*extraCombinationsNumber);
+   const manualColors = (()=>{
+      let manualFilters = 0
+      for (const f in config.filters) {
+         if (!Object.hasOwnProperty.call(config.filters, f)) return;
+         const filter = config.filters[f];
+         for (const k in filter) (Object.hasOwnProperty.call(filter, k) && k !== 'default') && manualFilters++
+      }
+      return baseNumber+filterNumber+manualFilters
+   })()
+   const meta = {numColors,manualColors,baseNumber,filterNumber,extraCombinationsNumber}
+
+   const applyColors= (base,variations)=>{
+      const color = new tinycolor(config.baseTokenColors[base])
+      variations.forEach(v=>{
+         const def = config.filters[v][base]||config.filters[v].default;
+         for (const k in def)((Object.hasOwnProperty.call(def, k)) && color[k](def[k]));
+      })
+      return color;
+   }
+
+   const encode=(color,text)=>{
+      const finalColor =   color.toHexString();
+      const finalText = text.replace(/\s+/g,'.');
+      let rule =  finalColor
+
+      const textMateTransform = r => {
+         if(typeof rule ==='string')return {foreground:rule};
+         let fontText = [];
+         if(r.bold)fontText.push('bold');
+         if(r.italic)fontText.push('italic');
+         if(r.bold === false && r.italic === false)fontText = [''];
+         return {foreground:r.foreground, fontStyle:fontText.join(' ')}
+      }
+
+      for (const f in config.textformatMapping) {
+         if (!(Object.hasOwnProperty.call(config.textformatMapping, f) && (new RegExp(`\\b${f}\\b`,'gm'))).test(finalText)) continue;
+         const formatDef = config.textformatMapping[f]
+         if(typeof rule === 'string')rule = {foreground:rule};
+         if(formatDef.clear) rule = {...rule, bold:false, italic:false};
+         else rule = {...rule,...formatDef}
+        }
+
+      semanticRules[finalText] = rule;
+      for (const k in config.alias) (Object.hasOwnProperty.call(config.alias, k) && (new RegExp(`\\b${k}\\b`,'gm')).test(finalText)) &&  config.alias[k].forEach(a=> semanticRules[finalText.replace(new RegExp(`\\b${k}\\b`, 'g'), a)] = (typeof rule === 'string'?{foreground:rule,alias:true}:{...rule,alias:true}) )
+      if(config.fallbacks[finalText]) fallBackRules.push({name:finalText,scope:config.fallbacks[finalText],settings:textMateTransform(rule)})
+   }
+
+   for (const t in config.baseTokenColors) {
+      if (!(Object.hasOwnProperty.call(config.baseTokenColors, t))) return;
+      encode(new tinycolor(config.baseTokenColors[t]),t)
+      for (const v in config.filters) ((Object.hasOwnProperty.call(config.filters, v)) && encode(applyColors(t,[v]),t+'\n'+v))
+      config.modifierCombinations.map(c=>c.split('.')).forEach(c=> encode(applyColors(t,c),t+'\n'+c.join(' ')))
+   }
+   return {semanticRules,fallBackRules,meta};
+}
+
+/**
+ * Ridiculously basic string interpolation to generate dynamic readme and html
+ * @param {string} text 
+ * @param {Record<string,any>} replacements 
+ * @returns 
+ */
+const interpolate = (text,replacements) => {
+   let finaltext = text
+   for (const r in replacements) {
+      if (!Object.prototype.hasOwnProperty.call(replacements, r)) continue;
+      finaltext = finaltext.replace(new RegExp(`\{${r}\}`, 'gim'), replacements[r])
+   } 
+   return finaltext;
+}
+
+export {generateColors,interpolate}
