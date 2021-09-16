04b9ac0d1346767a3ce7e48ed299b1af4369ba5f
Timo Hertzler <timo.hertzler@gmail.com>
2021-09-16 23:05:35 +0200

    readme improvements

--- generator/_Template.md
+++ generator/_Template.md
@@ -1,19 +1,25 @@
-![Banner](./assets/SR_Banner.png)
+![Banner](../assets/SR_Banner.png)
 
 # About
-Semantic highlighting 
+Plenty of themes offer minimalist highlighting focusing on specific aspects of code using a limited color palette. This theme takes the opposite approach.  
+Utilizing both bold colors and subtle distinctions, anything than *can* be identified *will* be identified.  
 
-![Banner](./assets/SR_Example.png)
+![Banner](../assets/SR_Example.png)
 
-{manualPercent}%
+Semantic Highlighting enables an amazing amount of granular control and **Semantic Rainbow** aims to exploit all of it.
+
+By highlighing **{baseNumber}** types of semantic tokens each with **{filterNumber}** varations corresponding to different semantic modifiers and an additional **{extraCombinationsNumber}** shades for combinations of two or more individual modifiers, this theme defines a total of **{numColors}** styles for every conceivable (and a few inconceivable) 
+
+If you've ever wanted a visual distinction between and and a *readonly async method*, and a *readonly async method of a default library* and also want to tell it apart from a *non-async default library method*
+
+## Background
+
+If {numColors} styles sounds like too much to configure by hand, you're right.  
+**Semantic Rainbow** is programmatically generated, based on a configuration of *base colors* for the different tokens and a list of specific *color transformations* for token modifiers which can also be stacked to represent different combinations.  
+About {manualPercent}% of styles/transformations are manually defined to ensure 
+
+## Compatibility
+The expressiveness of this theme is based on VSCode's Semantic Highlighing
+
 
 ## Contributing
-
-
-
-
-### For more information
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
--- generator/generate.js
+++ generator/generate.js
@@ -1,37 +1,38 @@
-//This file is used to parse the generated style color definitions into a valid
+//This file is used to merge the generated style rules with the actual VSCode theme definition as well as outputting the readme
 import tinycolor from "tinycolor2";
 import { generateColors, interpolate } from "./lib/colorGenerator.js";
 import {readFileSync,writeFile} from "fs";
-
+//Defining paths and reading files
 const configPath = './themes/Semantic Rainbow-color-theme.json';
 const readmeTemplatePath ='./generator/_Template.md';
 const readmePath ="./README.md"
-const cnfString =readFileSync(configPath,'utf-8');
-const readme = readFileSync(readmeTemplatePath,'utf-8');
-const cnf = JSON.parse(cnfString);
 const colorConfig = JSON.parse(readFileSync('./generator/config.json','utf-8'))
-
+const cnf =JSON.parse(readFileSync(configPath,'utf-8'));
+const readme = readFileSync(readmeTemplatePath,'utf-8');
+//Generating updated color values.
 const  {semanticRules, fallBackRules, meta} = generateColors(tinycolor,colorConfig);
 const {tokenColors} = cnf;
-
-fallBackRules.forEach(({scope})=>
- tokenColors.map((r,i)=>(typeof r.scope === 'string'?scope.includes(r.scope):r.scope.some(i => scope.includes(i)))?{i,r}:null).filter(f=>f).sort((a,b)=>a.i<b.i?1:-1).forEach(({r,i})=>{
-       if(scope.includes(r.scope)) return tokenColors.splice(i,1);
-       if(scope.every(i=>r.scope.includes(i) && (r.scope.length === scope.length))) return tokenColors.splice(i,1);
-       scope.forEach(s=>{
-          const subIndex = r.scope.findIndex(e=>e===s);
-          if(subIndex !== -1) tokenColors[i].scope = tokenColors[i].scope.splice(subIndex,1)
-       })
-    })
+//Here we deal with existing textmate rules that might interfere with the fallbacks that were generated
+fallBackRules.forEach(({scope}) => tokenColors.map((r,i)=>(typeof r.scope === 'string'?scope.includes(r.scope):r.scope.some(i => scope.includes(i)))?{i,r}:null).filter(f=>f).sort((a,b)=>a.i<b.i?1:-1).forEach(({r,i})=>{
+      //Deleting existing textmate rules that contradict our fallback rules
+      if(scope.includes(r.scope)) return tokenColors.splice(i,1);
+      if(scope.every(i=>r.scope.includes(i) && (r.scope.length === scope.length))) return tokenColors.splice(i,1);
+      //If any textmate rules contains individual scopes that are covered by out fallbacks they will be removed from the rule
+      scope.forEach(s=>{
+         const subIndex = r.scope.findIndex(e=>e===s);
+         if(subIndex !== -1) tokenColors[i].scope = tokenColors[i].scope.splice(subIndex,1)
+      })
+   })
 )
-
 //Deleting the "alias" property from rule definitions
 for (const k in semanticRules)((Object.hasOwnProperty.call(semanticRules, k)) && semanticRules[k].alias && delete semanticRules[k].alias)
-
+//Updating the color theme with the new values
 cnf.semanticTokenColors = semanticRules;
 cnf.tokenColors = tokenColors.concat(fallBackRules);
-
-const stringRules = JSON.stringify(cnf,null,3)
-
-if(stringRules !== cnfString)(writeFile(configPath,stringRules,'utf8',()=>{console.log('saved config.')}),writeFile(readmePath,interpolate(readme,meta),'utf8',()=>console.log('saved readme.')))
+const stringRules = JSON.stringify(cnf,null,3);
+//Updating the readme with color stats
+const finalReadme = interpolate(readme,meta).replace(/\]\(\.\./gmi,'](.');
+//Saving the files
+writeFile(configPath,stringRules,'utf8',()=>{console.log('saved config.')});
+writeFile(readmePath,finalReadme,'utf8',()=>console.log('saved readme.'));
 export {}
